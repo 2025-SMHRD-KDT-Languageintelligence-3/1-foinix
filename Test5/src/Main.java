8e50ac562b9a4bd22d2cfdfd65ca40fe8650807d
smhrd1 <smhrd1@192.168.219.211>
2025-06-05 12:44:35 +0900

    Merge branch 'master' of https://github.com/2025-SMHRD-KDT-Languageintelligence-3/1-foinix.git

--- Test5/src/Main.java
+++ Test5/src/Main.java
@@ -8,19 +8,19 @@ public class Main {
 		김승환 obj = new 김승환();
 		obj.show();
 		
-		김산해 obj = new 김산해();
+		김산해 obj1 = new 김산해();
 		obj.show();
 		
-		유미선 obj = new 유미선();
+		유미선 obj2 = new 유미선();
 		obj.show();
 		
-		나승주 obj = new 나승주();
+		나승주 obj3 = new 나승주();
 		obj.show();
 		
-		서광테 obj = new 서광테();
+		서광테 obj4 = new 서광테();
 		obj.show();
 		
-		손성주 obj = new 손성주();
+		손성주 obj5 = new 손성주();
 		obj.show();
 	}
 
